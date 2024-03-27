50b3c82ef990a6163733a93dc0455d731b4a4cc6
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-03-28 01:20:58 +0530

    model is ready for deployment

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, app,render_template
+from flask import Response
+import pickle
+import numpy as np
+import pandas as pd
+
+
+app  = Flask(__name__)
+
+scaler = pickle.load(open('./Model/scaler.pkl', 'rb'))
+model = pickle.load(open('./Model/logistic_regression.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods =['POST','GET'])
+def predict():
+    if request.method == 'POST':
+        Pregnancies=int(request.form.get("Pregnancies"))
+        Glucose = float(request.form.get('Glucose'))
+        BloodPressure = float(request.form.get('BloodPressure'))
+        SkinThickness = float(request.form.get('SkinThickness'))
+        Insulin = float(request.form.get('Insulin'))
+        BMI = float(request.form.get('BMI'))
+        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+        Age = float(request.form.get('Age'))
+        
+        new_data = scaler.transform([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        prediction = model.predict(new_data)
+        
+        if prediction == 1:
+            return render_template('index.html', prediction_text = 'The patient is diabetic')
+        else:
+            return render_template('index.html', prediction_text = 'The patient is not diabetic')
+        
+    else:
+        return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.run(debug = True)
